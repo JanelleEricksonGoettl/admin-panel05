20521a27f23aff51dda5b989a61999d392bd2a06
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-02-23 17:10:29 -0500

    configured Course controller routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  before_action :find_course, only: %i[show edit update]
+
   def index
     @courses = Course.all
   end
@@ -10,17 +12,42 @@ class CoursesController < ApplicationController
   def create
     @course = Course.new(course_params)
     if @course.save
-      msg
+      msg = "New course created."
+      flash.now[:success] = msg
+      redirect_to courses_path
+    else
+      msg = @course.errors.full_messages
+      flash.now[:error] = msg
+      render 'new'
+    end
   end
 
   def edit
   end
 
-  def show
+  def update
+    if @course.update
+      msg = "Course edited."
+      flash.now[:success] = msg
+      redirect_back(fallback_location: courses_path)
+    else
+      msg = @course.errors.full_messages
+      flash.now[:error] = msg
+      render 'edit'
+    end
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
 
   private
     def course_params
       params.require(:course).permit(:name, :hours)
+    end
+
+    def find_course
+      @course = Course.find(params[:id])
     end
 end
